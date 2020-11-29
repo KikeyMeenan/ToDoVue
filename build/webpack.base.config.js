73a31cfdadc760ced3078775990be05d3529d9ca
kikeymeenan <mikemk309@googlemail.com>
2020-11-29 10:32:36 +0000

    added hot module and linting

--- build/webpack.base.config.js
+++ build/webpack.base.config.js
@@ -4,13 +4,23 @@ module.exports = {
   entry: {
     app: path.resolve(__dirname, "../src/app.js")
   },
-  output: {
-    path: path.resolve(__dirname, '../dist'),
-    filename: 'assets/js/[name].js'
+  module: {
+    rules: [
+      {
+        enforce: "pre",
+        test: /(\.js$)/,
+        loader: "eslint-loader",
+        exclude: /node_modules/
+      }
+    ]
   },
   resolve: {
     alias: {
       vue: "vue/dist/vue.js"
     }
+  },
+  output: {
+    path: path.resolve(__dirname, '../dist'),
+    filename: 'assets/js/[name].js'
   }
 };
--- src/app.js
+++ src/app.js
@@ -1,7 +1,12 @@
-import Vue from "vue";
+import Vue from 'vue'
 
 new Vue({
   data: {
-    hello: 'hi there'
-  }
-}).$mount("#app");
+    hello: 'hi there xccx'
+  },
+  template: '<div id="app">{{ hello }}</div>'
+}).$mount('#app')
+
+if (module.hot) {
+  module.hot.accept()
+}
