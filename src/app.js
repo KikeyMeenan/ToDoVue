a52632fc96431abc3326fe6c490f9e4f86547f60
kikeymeenan <mikemk309@googlemail.com>
2020-11-30 14:16:56 +0000

    give up and use vue cli

--- src/app.js
+++
@@ -1,6 +0,0 @@
-import Vue from 'vue'
-import AppLayout from './theme/Layout.vue'
-
-new Vue({
-  ...AppLayout
-}).$mount('#app')
--- src/theme/Layout.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div>
-    template here xocp
-  </div>
-</template>
-<script>
-export default { }
-</script>
