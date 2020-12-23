0ea32dac41674d4ad0f1ce849b79db46afb18d7b
kikeymeenan <mikemk309@googlemail.com>
2020-12-23 14:50:14 +0000

    added basic font

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,54 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <!-- would be better to have sub-routers,
-      have list links in main,
-      then create/edit as children -->
-      <router-link to="/">To Do</router-link> |
-      <router-link to="/create-item">Create Item</router-link> |
-      <router-link to="/categories">Categories</router-link> |
-      <router-link to="/create-category">Create Category</router-link> |
+    <div class="nav">
+      <router-link class="nav__item" to="/">To Do</router-link>
+      <router-link class="nav__item" to="/create-item">Create Item</router-link>
+      <router-link class="nav__item" to="/categories">Categories</router-link>
+      <router-link class="nav__item" to="/create-category">Create Category</router-link>
     </div>
     <router-view/>
+    <LoadingMessage />
+    <ActionMessage id="action-message-container" />
   </div>
 </template>
+
+<script lang="ts">
+import ActionMessage from '@/components/ActionMessage/ActionMessage.vue';
+import LoadingMessage from '@/components/LoadingMessage/LoadingMessage.vue';
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component({
+  components: {
+    ActionMessage,
+    LoadingMessage,
+  },
+})
+export default class CreateItem extends Vue {}
+</script>
+
+<style scoped lang="scss">
+.nav {
+  width: 100%;
+  padding-top:10px;
+  padding-bottom: 10px;
+  border-bottom: 1px solid #000;
+
+  &__item {
+    padding-right: 10px;
+
+    &:not(:first-child) {
+      padding-left: 10px;
+    }
+
+    &:not(:last-child) {
+      border-right: 1px solid #000;
+    }
+  }
+}
+
+#action-message-container {
+  position: fixed;
+  right: 0;
+  bottom: 0;
+}
+</style>
